ba4eebcda22a03daa2ceaa711e81d15e428099f6
Krym <gumimash@gmail.com>
2024-01-05 15:20:58 +0200

    Finished navbar

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,9 +1,29 @@
 import React, { useState } from "react";
-import { RiMenu3Line, RiCloseLine, RiMenu3Line } from "react-icons/ri";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./navbar.css";
 import logo from "../../assets/logo.svg";
 
-const navbar = () => {
+const Menu = () => (
+  <>
+    <p>
+      <a href="#home">Home</a>
+    </p>
+    <p>
+      <a href="#wgpt3">What is GPT3?</a>
+    </p>
+    <p>
+      <a href="#possibility">Open AI</a>
+    </p>
+    <p>
+      <a href="#features">Case Studies</a>
+    </p>
+    <p>
+      <a href="#blog">Library</a>
+    </p>
+  </>
+);
+
+const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className="gpt3__navbar">
@@ -12,21 +32,7 @@ const navbar = () => {
           <img src={logo} alt="logo" />
         </div>
         <div className="gpt3__navbar-links_container">
-          <p>
-            <a href="#home">Home</a>
-          </p>
-          <p>
-            <a href="#wgpt3">What is GPT3?</a>
-          </p>
-          <p>
-            <a href="#possibility">Open AI</a>
-          </p>
-          <p>
-            <a href="#features">Case Studies</a>
-          </p>
-          <p>
-            <a href="#blog">Library</a>
-          </p>
+          <Menu />
         </div>
       </div>
       <div className="gpt3__navbar-sign">
@@ -49,7 +55,13 @@ const navbar = () => {
         )}
         {toggleMenu && (
           <div className="gpt3__navbar-menu_container scale-up-center">
-            <div className="gpt3_navbar-menu_container-links"></div>
+            <div className="gpt3__navbar-menu_container-links">
+              <Menu />;
+              <div className="gpt3__navbar-menu_container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
           </div>
         )}
       </div>
@@ -57,4 +69,4 @@ const navbar = () => {
   );
 };
 
-export default navbar;
+export default Navbar;
