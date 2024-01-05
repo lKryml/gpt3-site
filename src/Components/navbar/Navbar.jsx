af4f1e0ce616152ba11b1805c8f803dc2eef96b4
Krym <gumimash@gmail.com>
2024-01-05 12:09:07 +0200

    Init

---
+++ src/Components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {RiMenu3Lin, RiCloseLin} from "react-icons/ri"
+import "./navbar.css";
+const navbar = () => {
+  return (
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links_logo'>
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default navbar
