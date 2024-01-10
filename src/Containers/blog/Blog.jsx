f1e8d0b4a704dc441e713492c1bd169843933ee1
Krym <gumimash@gmail.com>
2024-01-10 08:30:19 +0200

    Finished blog sect

--- src/Containers/blog/Blog.jsx
+++ src/Containers/blog/Blog.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import "./blog.css";
-const blog = () => {
+import { Article } from '../../Components'
+import { blog01, blog02, blog03, blog04, blog05 } from "./imports.js"
+
+const Blog = () => {
   return (
-    <div>blog</div>
+    <div className='gpt3__blog section__padding' id="blog">
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>A lot is happening We are blogging about it.</h1>
+      </div>
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container_groupA'>
+          <Article imgUrl={blog01} date={"1 Nov 2023"} title={"Bozo"} />
+        </div>
+        <div className='gpt3__blog-container_groupB'>
+          <Article imgUrl={blog02} date={"1 Nov 2023"} title={"Bozo"} />
+          <Article imgUrl={blog03} date={"1 Nov 2023"} title={"Bozo"} />
+          <Article imgUrl={blog04} date={"1 Nov 2023"} title={"Bozo"} />
+          <Article imgUrl={blog05} date={"1 Nov 2023"} title={"Bozo"} />
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default blog
+export default Blog
